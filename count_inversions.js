e80a60b74100c0f2d61e2cc5ced40620566348e5
Timur Morozov <radist@desant.biz>
2013-07-07 20:40:48 +0000

    init

---
+++ count_inversions.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var program = require('commander');
+var INPUT_FILE_DEFAULT = "checks.json";
+
+var assertFileExists = function(infile) {
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
+};
+
+
+var clone = function(fn) {
+    // Workaround for commander.js issue.
+    // http://stackoverflow.com/a/6772648
+    return fn.bind({});
+};
+
+if(require.main == module) {
+  program
+    .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+    .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <url>', 'URL of data to check')
+    .parse(process.argv);
+
+} else {
+    exports.checkHtmlFile = checkHtmlFile;
+}
